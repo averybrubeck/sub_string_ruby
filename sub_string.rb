9bf67eb02dca514fe9b76136918929bb36b6dea9
averybrubeck <averybrubeck@Yahoo.com>
2023-09-26 12:44:40 -0400

    project setup

---
+++ sub_string.rb
@@ -0,0 +1,15 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def sub_string
+
+    string = gets.chomp.to_s.downcase
+    puts string
+
+end
+
+
+sub_string
+
+
+    #scan string for matching chars'
+    #print matching char's with number of times they occur 
