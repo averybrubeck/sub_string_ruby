0c652b77398e52513a8ec517858ad52f63f95ac9
averybrubeck <averybrubeck@yahoo.com>
2023-10-26 20:12:29 -0400

    added user input refactored code

--- sub_string.rb
+++ sub_string.rb
@@ -1,15 +1,15 @@
 def substrings(text, dictionary)
+    puts "Enter Text:"
+    lowered = gets.chomp.to_s
+    lowered = lowered.downcase.gsub(/[^a-z0-9\s]/i, '')
     result = Hash.new(0)
-    lowered = text.downcase
-    
+
     dictionary.each do |word|
         matches = lowered.scan(word).length
         result[word] = matches unless matches == 0
     end      
     puts result
 end
-
-
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings("Howdy partner, sit down! How's it going?", dictionary)
